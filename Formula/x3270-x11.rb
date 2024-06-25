78fd40302a963eb1b753d3bdd80b81eedd0dcbdf
Matteo Panella <matteo@panella.io>
2024-06-26 00:01:38 +0200

    Add x3270-x11

---
+++ Formula/x3270-x11.rb
@@ -0,0 +1,50 @@
+class X3270X11 < Formula
+  desc "IBM 3270 terminal emulator for the X Window System and Windows"
+  homepage "http://x3270.bgp.nu/"
+  url "http://x3270.bgp.nu/download/04.03/suite3270-4.3ga8-src.tgz"
+  sha256 "81c0ba4447d97a7b483c40e11b39d4498bbc9af55fa4f78ccff064b3e378dc59"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://x3270.miraheze.org/wiki/Downloads"
+    regex(/href=.*?suite3270[._-]v?(\d+(?:\.\d+)+(?:ga\d+)?)(?:-src)?\.t/i)
+  end
+
+  depends_on "openssl@3"
+  depends_on "readline"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "libice"
+  depends_on "libx11"
+  depends_on "libxaw"
+  depends_on "libxext"
+  depends_on "libxft"
+  depends_on "libxinerama"
+  depends_on "libxmu"
+  depends_on "libxpm"
+  depends_on "libxt"
+  depends_on "mkfontscale" => :build
+
+  conflicts_with "x3270", because: "x3270-x11 also provides the same binaries as x3270"
+
+  uses_from_macos "tcl-tk"
+
+  def install
+    ENV.append "CPPFLAGS", "-I#{Formula["tcl-tk"].opt_include}/tcl-tk" unless OS.mac?
+
+    args = %w[
+      --enable-c3270
+      --enable-pr3287
+      --enable-s3270
+      --enable-tcl3270
+      --enable-x3270
+    ]
+    system "./configure", *std_configure_args, *args
+    system "make", "install"
+    system "make", "install.man"
+  end
+
+  test do
+    system bin/"c3270", "--version"
+  end
+end
